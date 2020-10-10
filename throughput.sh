13629edd055583b02572ab1b95efc896f96799b3
Curt Dodds <dodds@hawaii.edu>
2020-10-10 10:54:03 -1000

    Add counting threads

--- throughput.sh
+++ throughput.sh
@@ -1,1 +1,17 @@
-grep size wget.log|./diskthru 8
+#!/bin/bash
+# Curt Dodds
+# October 9, 2020
+
+# auto build diskthru
+test -e diskthru || make diskthru
+if test ! -e diskthru
+then
+  echo Could not build diskthru ... exiting
+  exit 1
+fi
+
+# get thread count by counting manifest files
+THREADS=`ls manifest_* | wc -l`
+
+# get the final download stats from the log and feed to diskthru
+grep size wget.log|./diskthru $THREADS
