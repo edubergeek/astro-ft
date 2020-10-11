3c34b7c2b023faae1142840961690784e7bb0324
Curt Dodds <dodds@hawaii.edu>
2020-10-10 19:13:15 -1000

    Heavily modified the script to handle downloads from differing indexes Specifically: dtn-itc.ifa.hawaii.edu and archive.stsci.edu (MAST at STscI PS1-STRM catalog)

--- make-manifest.sh
+++ make-manifest.sh
@@ -8,6 +8,28 @@ then
   echo "You must specific a URL to download from. Exiting."
   exit 1
 fi
+SERVER=`expr $URL : 'http[s]*://\([^/]*\)'`
+
+default_pattern() {
+#  grep "href=\"$SERVER"
+  grep href= | egrep '.csv|.zg|.tgz'
+}
+
+match_pattern() {
+  case $URL in
+#  http://archive.stsci.edu/*)
+#    mast_pattern
+#    ;;
+  *)
+    default_pattern
+    ;;
+  esac
+}
+
+strip_server() {
+  sed -e "s/^http[s]*:\/\/$SERVER//"
+  echo $?
+}
 
 #<title>Index of /ps1/wise-photo-z</title>
 
@@ -15,11 +37,20 @@ fi
 cp /dev/null manifest
 
 # generate a list of files in the web directory
-curl $URL 2>/dev/null | awk '{print $5}' | grep '^href' | tail -n +3 | sed -e 's/^href.*"\(.*\)".*$/\1/' >mani-list
+#curl $URL 2>/dev/null | awk '{print $5}' | match_pattern >href-list
+curl -o html-list $URL
+match_pattern <html-list >href-list
+sed -e 's/.*href="\([htps]*[^"]*\)".*$/\1/' <href-list | strip_server >mani-list
+
+# strip URL down to just the web root URL
+# the rest of the URL is in each row in mani-list
+# only do this if the href-list contains http:
+grep 'http[s]*://' html-list >/dev/null 2>&1 && URL=`expr $URL : '\(http[s]*://[^/]*\)'`
+
 cat mani-list | while read f
 do
   fsize=`curl -L -I $URL/$f 2>/dev/null | grep Content-Length | awk '{print $2}'`
-  echo $URL $f $fsize | tr -d "\r" >>manifest
-done
-rm -f mani-list
+  echo $URL $f $fsize | tr -d "\r"
+done | awk '{if(NF==3) print ;}' >manifest
+rm -f mani-list href-list html-list
 exit 0
