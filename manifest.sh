1f7688d0279f754ac86e7b7792a9d563d4d088d6
Curt Dodds <dodds@hawaii.edu>
2022-03-11 19:28:25 +0000

    Fix bug of missing files due to divide by thread remainder

--- manifest.sh
+++ manifest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Curt Dodds
-# October 9, 2020
+# 2020-10-09 Initial version
+# 2021-12-06 First cut at spider mode
 
 # wipe any prior mannifest files but keep the master manifest
 rm -f manifest_*
@@ -14,13 +15,23 @@ threads=${1:-8}
 echo $threads threads
 n=`expr $N / $threads`
 echo $n files per thread
+x=`expr $N - $threads \* $n`
+echo with $x files remaining
 
 # split the manifest into per thread manifest files
 # for fetching from a proxy server it would be helpful to have prior knowledge
 # which files are cached on which servers i
 # e.g. the PSPS slice servers proxied through web02
+y=0
 for((t=0;t<$threads;t++))
 do
-  start=`expr $n \* $t + $n`
-  head -$start manifest | tail -$n >manifest_$t
+  if test $y -lt $x
+  then
+    y=`expr $y + 1`
+    m=`expr $n + 1`
+  else
+    m=$n
+  fi
+  start=`expr $n \* $t + $n + $y`
+  head -$start manifest | tail -$m >manifest_$t
 done
